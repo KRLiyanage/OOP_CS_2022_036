305ec53167c8cda0d15ef4753fccbc1819232478
KRLiyanage <kaweeshravishanka8@gmail.com>
2025-09-25 19:54:30 +0530

    init commit

---
+++ src/LW_02/Q_04.java
@@ -0,0 +1,34 @@
+package LW_02;
+
+import java.util.Scanner;
+
+public class Q_04 {
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        int max=0;
+        int secMax=0;
+        int maxN=0;
+
+        Scanner x = new Scanner(System.in);
+
+        System.out.println("Enter Values:");
+        for (int i=0;i< arr.length;i++){
+            arr[i]=x.nextInt();
+        }
+        for (int j=0;j<5;j++){
+            if(max < arr[j]){
+                max=arr[j];
+                maxN=j;
+            }
+        }
+        for (int k=0;k<5;k++){
+            if(k!=maxN){
+                if (secMax < arr[k]){
+                    secMax=arr[k];
+                }
+            }
+        }
+
+        System.out.println("The Second-largest Element is "+secMax);
+    }
+}
