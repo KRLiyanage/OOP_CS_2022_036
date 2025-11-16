35ffbf9d656a2313ce134a9f817a7145c0d1cbbd
KRLiyanage <kaweeshravishanka8@gmail.com>
2025-11-16 22:27:10 +0530

    init commit

---
+++ src/LW_04/Q_06.java
@@ -0,0 +1,12 @@
+package LW_04;
+
+public class Q_06 {
+    public static void main(String[] args) {
+        for (int i =1;i<=4;i++){
+            for (int j=0;j<=9;j++){
+                System.out.print(i+""+j+" ");
+            }
+            System.out.println("");
+        }
+    }
+}
