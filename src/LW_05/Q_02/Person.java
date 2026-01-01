a95cd18cfbcff90564b60c71543796f735b06878
KRLiyanage <kaweeshravishanka8@gmail.com>
2026-01-01 09:48:17 +0530

    init commit

---
+++ src/LW_05/Q_02/Person.java
@@ -0,0 +1,21 @@
+package LW_05.Q_02;
+
+public abstract class Person {
+
+    private String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public abstract void displayInfo();
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
