fa180833e79ec500a35c08e85b6e6a1e4769858e
KRLiyanage <kaweeshravishanka8@gmail.com>
2026-01-02 22:32:41 +0530

    init commit

--- src/LW_08/SingUp.java
+++ src/LW_08/SingUp.java
@@ -19,7 +19,7 @@ public class SingUp extends JFrame implements ActionListener {
     final String[] months = {"January","February","March","April","May","June","July","August","September","October","November","December"};
     final String[] years = {"1990","1991","1992","1993","1994","1995","1996","1997","1998","1999","2000","2001","2002","2003","2004","2005","2006","2007","2008","2009","2010"};
 
-    SingUp(){
+    public SingUp(){
 
         setTitle("Registration Form");
         setBounds(570,140,400,600);
